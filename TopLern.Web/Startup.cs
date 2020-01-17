6188e57773f7172ea267113a456cd530b6d45c85
alireza asgary <asgary_alireza@hotmail.com>
2020-01-17 17:18:27 +0330

    part3

--- TopLern.Web/Startup.cs
+++ TopLern.Web/Startup.cs
@@ -5,17 +5,36 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using TopLearn.DataLayer.Context;
 
 namespace TopLern.Web
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            #region DataBase Context
+
+            services.AddDbContext<TopLearnContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("TopLearnConnection"));
+            }
+            );
+
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
