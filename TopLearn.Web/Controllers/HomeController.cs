0578d97d2b7cd19aeeadbf58bca35808c66d1a78
Alireza Asgary <alireza2010asgary@gmail.com>
2020-01-29 15:57:30 +0330

    add site project

---
+++ TopLearn.Web/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TopLearn.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+
+        [Authorize]
+        public IActionResult Test() => View();
+    }
+}
